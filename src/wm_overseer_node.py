e510c9b9e28a2781fd0c777d42d3b59b80901eb6
NuuJoy <63086412+NuuJoy@users.noreply.github.com>
2020-05-07 17:29:27 +0700

    Update wm_overseer_node.py

--- src/wm_overseer_node.py
+++ src/wm_overseer_node.py
@@ -5,9 +5,18 @@ import json
 import rospy
 import std_msgs.msg
 
+class overseer():
+    def __init__(self,hardwareDict):
+        self.hardwareDict = hardwareDict
+        self.status_node  = rospy.Publisher('/'+ generalsetup['machineName'] +'/overseer/status', std_msgs.msg.Bool, queue_size=100)
+
+    def executeFile(self,jobfilename):
+        with open(jobfilename,'r') as jobfile:
+            joblist = json.loads(jobfile.read())
+
+            
 
 if __name__ == '__main__':
-
     try:
         if len(sys.argv) > 1:
             setupFileName = sys.argv[1]
@@ -26,20 +35,8 @@ if __name__ == '__main__':
         hardwareDict['valvectrl_state'] = rospy.Publisher('/'+ generalsetup['machineName'] +'/hwctrl/valvectrl/state', std_msgs.msg.Bool, queue_size=100)
         hardwareDict['pumpctrl_state']  = rospy.Publisher('/'+ generalsetup['machineName'] +'/hwctrl/pumpctrl/state', std_msgs.msg.Bool, queue_size=100)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        overseer_worker = overseer(hardwareDict)
+        overseer_input  = rospy.Subscriber('/'+ generalsetup['machineName'] +'/overseer/inputfile', std_msgs.msg.Bool, overseer_worker.executeFile, queue_size=100)
 
         rospy.spin()
 
