5b9cbf88d3d53a932a91846f9bde1ea821ff0667
NuuJoy <63086412+NuuJoy@users.noreply.github.com>
2020-05-12 18:40:50 +0700

    Update wm_overseer_node.py

--- src/wm_overseer_node.py
+++ src/wm_overseer_node.py
@@ -2,29 +2,47 @@
 
 import sys
 import json
+import time
 import rospy
 import std_msgs.msg
 
 class overseer():
-    def __init__(self,hardwareDict):
-        self.hardwareDict = hardwareDict
-        self.status_node  = rospy.Publisher('/'+ generalsetup['machineName'] +'/overseer/status', std_msgs.msg.Bool, queue_size=100)
+    def __init__(self,hardwareDict,overseer_delay):
+        self.hardwareDict   = hardwareDict
+        self.overseer_delay = overseer_delay
 
-    def executeFile(self,jobfilename):
-        with open(jobfilename,'r') as jobfile:
+    def executeFile(self,jobfilepath):
+        with open(jobfilepath,'r') as jobfile:
+            # joblist
+            #     command:
+            #         pwmSetDeg([<string:nodename>,<float:degree>])
+            #         pwmActive(<bool:on/off>)
+            #         valveActive(<bool:on/off>)
+            #         pumpActive(<bool:on/off>)
+            #         delay(<float:delat_sec>)
+            #     format:
+            #         [<string:command>,<list:argv>]
             joblist = json.loads(jobfile.read())
 
-            
+            for (cmd,argv) in joblist:
+                if cmd == 'pwmSetDeg':
+                    self.hardwareDict[argv[0]].publish(argv[1])
+                elif cmd == 'pwmActive':
+                    self.hardwareDict['pwmctrl_state'].publish(argv)
+                elif cmd == 'valveActive':
+                    self.hardwareDict['valvectrl_state'].publish(argv)
+                elif cmd == 'pumpActive':
+                    self.hardwareDict['pumpctrl_state'].publish(argv)
+                elif cmd == 'delay':
+                    time.sleep(argv)
+
+                time.sleep(self.overseer_delay)
 
 if __name__ == '__main__':
     try:
-        if len(sys.argv) > 1:
-            setupFileName = sys.argv[1]
-        else:
-            setupFileName = 'wm_setupFile.json'
-
+        setupFileName = sys.argv[1] if len(sys.argv) > 1 else 'wm_setupFile.json'
         with open(setupFileName,'r') as setupFile:
-            generalsetup, gpiosetup, pwmsetup = json.loads(setupFile.read())
+            generalsetup,_,pwmsetup,_,_ = json.loads(setupFile.read())
 
         rospy.init_node(generalsetup['machineName']+'_overseer_node', anonymous=True)
         
@@ -35,8 +53,9 @@ if __name__ == '__main__':
         hardwareDict['valvectrl_state'] = rospy.Publisher('/'+ generalsetup['machineName'] +'/hwctrl/valvectrl/state', std_msgs.msg.Bool, queue_size=100)
         hardwareDict['pumpctrl_state']  = rospy.Publisher('/'+ generalsetup['machineName'] +'/hwctrl/pumpctrl/state', std_msgs.msg.Bool, queue_size=100)
 
-        overseer_worker = overseer(hardwareDict)
-        overseer_input  = rospy.Subscriber('/'+ generalsetup['machineName'] +'/overseer/inputfile', std_msgs.msg.Bool, overseer_worker.executeFile, queue_size=100)
+        overseer_worker = overseer(hardwareDict,generalsetup['overseer_delay'])
+        
+        rospy.Subscriber('/'+ generalsetup['machineName'] +'/overseer/inputfile', std_msgs.msg.String, overseer_worker.executeFile, queue_size=100)
 
         rospy.spin()
 
