cfe2f55710d77d3bf51bd94ca315a2c239001296
NuuJoy <63086412+NuuJoy@users.noreply.github.com>
2020-05-13 15:06:47 +0700

    Update setupfile_gen.py

--- setupfile_gen.py
+++ setupfile_gen.py
@@ -1,7 +1,7 @@
 import json
 
 generalsetup = {'machineName':'wm_ros',
-                'overseer_delay':0.005}
+                'overseer_delay':0.01}
 gpiosetup    = [11,13,15]
 pwmsetup     = [['baseazim',0,205,410,-90.0,90.0],
                 ['basealti',0,205,410,-90.0,90.0],
@@ -13,7 +13,10 @@ envsensetup  = [['humid_left' , 'DHT11', [17]],
                 ['illum_left' , 'BH1750', [0,False]],
                 ['illum_mid'  , 'BH1750', [0,False]],
                 ['illum_right', 'BH1750', [0,False]]]
-managersetup = []
+managertask  = [['Mon',8,30,'DoSomething.json',180],
+                ['Tue',15,0,'DoSomeOtherthing.json',60],
+                ['Wed',20,15,'DoAnotherthing.json',45],
+                ['Wed',12,0,'DoNothing.json'],600]
 
 with open('wm_setupFile.json','w') as outputFile:
-    outputFile.write(json.dumps([generalsetup,gpiosetup,pwmsetup,envsensetup,managersetup]))
+    outputFile.write(json.dumps([generalsetup,gpiosetup,pwmsetup,envsensetup,managertask]))
