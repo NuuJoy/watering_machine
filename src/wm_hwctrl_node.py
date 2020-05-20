4d64af143288ca3f32104ca0a0be2d43e31f0138
NuuJoy <63086412+NuuJoy@users.noreply.github.com>
2020-05-20 17:27:36 +0700

    Update wm_hwctrl_node.py

--- src/wm_hwctrl_node.py
+++ src/wm_hwctrl_node.py
@@ -7,63 +7,72 @@ import RPi.GPIO
 import Adafruit_PCA9685
 
 class pwmDriver():
-    def __init__(self,nodeName,pinNum,minWdth,maxWdth,minDeg,maxDeg):
+    def __init__(self,pinNum,wdth1,deg1,wdth2,deg2):
         # init self parameter
-        self.nodeName = nodeName
-        self.pinNum   = pinNum
-        self.minWdth  = minWdth
-        self.maxWdth  = maxWdth
-        self.minDeg   = minDeg
-        self.maxDeg   = maxDeg
-        self.curDeg   = None
-        self.pwm = Adafruit_PCA9685.PCA9685()
-        self.pwm.set_pwm_freq(50) # set frequency 50 Hz
+        self.pinNum = pinNum
+        self.wdth1  = wdth1
+        self.wdth2  = wdth2
+        self.deg1   = deg1
+        self.deg2   = deg2
+        self._pwm = Adafruit_PCA9685.PCA9685()
+        self._pwm.set_pwm_freq(50) # set frequency 50 Hz
+    
+    def deg2wdth_func(self,input_deg):
+        # just linear interpolation from y=mx+c
+        return round((self.wdth2-self.wdth1)/(self.deg2-self.deg1)*input_deg+(self.wdth1-(self.wdth2-self.wdth1)/(self.deg2-self.deg1)*self.deg1))
 
     def setPWM(self,rawSetDeg):
-        # adjust if setDeg is exceed limit
-        setDeg = self.minDeg if rawSetDeg.data < self.minDeg else rawSetDeg.data
-        setDeg = self.maxDeg if rawSetDeg.data > self.maxDeg else rawSetDeg.data
+        # clamp setDeg value to limit
+        setDeg = max(min(max(self.deg1,self.deg2), rawSetDeg.data), min(self.deg1,self.deg2))
         # calculate degree to int(4096)
-        setPcnt  = (setDeg-self.minDeg)/(self.maxDeg-self.minDeg)
-        setInput = int(setPcnt*(self.maxWdth-self.minWdth) + self.minWdth)
+        setInt = self.deg2wdth_func(setDeg)
         # set servo angle
-        self.pwm.set_pwm(self.pinNum, 0, setInput)
+        self._pwm.set_pwm(self.pinNum, 0, setInt)
 
 class gpioDriver():
     def __init__(self,pwmena_pin,valve_pin,pump_pin):
+        # init RPi.GPIO
+        RPi.GPIO.setmode(RPi.GPIO.BOARD)
+        RPi.GPIO.setwarnings(False)
         # pin setup
         self.pwmena_pin = pwmena_pin
         self.valve_pin  = valve_pin
         self.pump_pin   = pump_pin
 
-        # init RPi.GPIO
-        RPi.GPIO.setmode(RPi.GPIO.BOARD)
-        RPi.GPIO.setwarnings(False)
-
-        # init with pwm disable, valve disable
-        RPi.GPIO.setup(self.pwmena_pin, RPi.GPIO.OUT, initial=RPi.GPIO.HIGH)
-        RPi.GPIO.setup(self.valve_pin, RPi.GPIO.OUT, initial=RPi.GPIO.LOW)
+    @property
+    def pwmena_pin(self):
+        return self.__pwmena_pin
+    @pwmena_pin.setter
+    def pwmena_pin(self, pwmena_pin):
+        if hasattr(self,'__pwmena_pin'):
+            RPi.GPIO.cleanup(self.__pwmena_pin)
+        self.__pwmena_pin = pwmena_pin
+        RPi.GPIO.setup(self.__pwmena_pin, RPi.GPIO.OUT, initial=RPi.GPIO.HIGH)
+    @property
+    def valve_pin(self):
+        return self.__valve_pin
+    @valve_pin.setter
+    def valve_pin(self, valve_pin):
+        if hasattr(self,'__valve_pin'):
+            RPi.GPIO.cleanup(self.__valve_pin)
+        self.__valve_pin = valve_pin
+        RPi.GPIO.setup(self.__valve_pin, RPi.GPIO.OUT, initial=RPi.GPIO.LOW)
+    @property
+    def pump_pin(self):
+        return self.__pump_pin
+    @pump_pin.setter
+    def pump_pin(self, pump_pin):
+        if hasattr(self,'__pump_pin'):
+            RPi.GPIO.cleanup(self.__pump_pin)
+        self.__pump_pin = pump_pin
+        RPi.GPIO.setup(self.__pump_pin, RPi.GPIO.OUT, initial=RPi.GPIO.LOW)
 
     def pwm_active_set(self,pwm_active_status):
-        # enable = LOW
-        if pwm_active_status:
-            RPi.GPIO.output(self.pwmena_pin, RPi.GPIO.LOW)
-        else:
-            RPi.GPIO.output(self.pwmena_pin, RPi.GPIO.HIGH)
-
+        RPi.GPIO.output(self.pwmena_pin, [RPi.GPIO.HIGH,RPi.GPIO.LOW][pwm_active_status.data])  # false=high, true=low
     def valve_active_set(self,valve_active_status):
-        # enable = HIGH
-        if valve_active_status:
-            RPi.GPIO.output(self.valve_pin, RPi.GPIO.HIGH)
-        else:
-            RPi.GPIO.output(self.valve_pin, RPi.GPIO.LOW)
-
+        RPi.GPIO.output(self.valve_pin, [RPi.GPIO.LOW,RPi.GPIO.HIGH][valve_active_status.data]) # false=low, true=high
     def pump_active_set(self,pump_active_status):
-        # enable = HIGH
-        if pump_active_status:
-            RPi.GPIO.output(self.pump_pin, RPi.GPIO.HIGH)
-        else:
-            RPi.GPIO.output(self.pump_pin, RPi.GPIO.LOW)
+        RPi.GPIO.output(self.pump_pin, [RPi.GPIO.LOW,RPi.GPIO.HIGH][pump_active_status.data])   # false=low, true=high
 
 if __name__ == '__main__':
     try:
@@ -71,11 +80,11 @@ if __name__ == '__main__':
 
         actuatorsDict = {}
 
-        baseazim = pwmDriver('baseazim', 0, 205, 410, -90, 90)
-        basealti = pwmDriver('basealti', 1, 205, 410, -90, 90)
-        midnode  = pwmDriver('midnode' , 2, 205, 410, -90, 90)
-        endnode  = pwmDriver('endnode' , 3, 205, 410, -90, 90)
-        closure  = pwmDriver('closure' ,15, 205, 410,   0, 90)
+        baseazim = pwmDriver( 0, 205,-90, 410, 90)
+        basealti = pwmDriver( 1, 205,-90, 410, 90)
+        midnode  = pwmDriver( 2, 205,-90, 410, 90)
+        endnode  = pwmDriver( 3, 205,-90, 410, 90)
+        closure  = pwmDriver(15, 205,  0, 410, 90)
 
         rospy.Subscriber('/wm_ros/hwctrl/pwmctrl/baseazim/setdeg', std_msgs.msg.Float64, baseazim.setPWM, queue_size=100)
         rospy.Subscriber('/wm_ros/hwctrl/pwmctrl/basealti/setdeg', std_msgs.msg.Float64, basealti.setPWM, queue_size=100)
@@ -83,7 +92,7 @@ if __name__ == '__main__':
         rospy.Subscriber('/wm_ros/hwctrl/pwmctrl/endnode/setdeg' , std_msgs.msg.Float64, endnode.setPWM , queue_size=100)
         rospy.Subscriber('/wm_ros/hwctrl/pwmctrl/closure/setdeg' , std_msgs.msg.Float64, closure.setPWM , queue_size=100)
 
-        switchDict = gpioDriver(27,28,29)
+        switchDict = gpioDriver(33,35,37)
         rospy.Subscriber('/wm_ros/hwctrl/pwmctrl/state'  , std_msgs.msg.Bool, switchDict.pwm_active_set  , queue_size=100)
         rospy.Subscriber('/wm_ros/hwctrl/valvectrl/state', std_msgs.msg.Bool, switchDict.valve_active_set, queue_size=100)
         rospy.Subscriber('/wm_ros/hwctrl/pumpctrl/state' , std_msgs.msg.Bool, switchDict.pump_active_set , queue_size=100)
@@ -94,4 +103,4 @@ if __name__ == '__main__':
         # clean-up pwm and gpio on exit
         for actuator in actuatorsDict:
             actuator.pwm.set_pwm(actuator.pinNum, 0, 0)
-        RPi.GPIO.cleanup([switchDict.pwmena_pin,switchDict.valve_pin,switchDict.pump_pin])
+        RPi.GPIO.cleanup([switchDict._pwmenaPin,switchDict._valvePin,switchDict._pumpPin])
